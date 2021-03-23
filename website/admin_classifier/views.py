6d8d7f0cbb6eb4ea8cfd551a5662c8f7dd922c57
Poojan Vyas <poojan.vyas08@gmail.com>
2021-03-22 19:15:54 -0600

    Simple Factory Pattern Intigrated

--- website/admin_classifier/views.py
+++ website/admin_classifier/views.py
@@ -5,16 +5,12 @@ from io import StringIO
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score
 from django.views import View
-from pymongo import MongoClient
 from bson.binary import Binary
 from base64 import b64encode
+from . import mongodb as mdb
 
 data = pd.DataFrame({})
-client = MongoClient("mongodb+srv://user_1:USER_1@cluster0.0oqke.mongodb.net/<dbname>?retryWrites=true&w=majority")
-db = client.get_database('learnml_db')
-db_data = db.algorithms
-
-# Create your views here.
+db_data = mdb.access()
 
 
 class Algorithm(View):
@@ -25,6 +21,39 @@ class Algorithm(View):
     def post(self, request):
         pass
 
+    @staticmethod
+    def description_update(algo_desc, ds_desc, algo_name):
+        update_data = {'algo_desc': str(algo_desc), 'ds_desc': str(ds_desc)}
+        update_message = mdb.update(db_data, algo_name, update_data, "Descriptions Updated",
+                                    "Unexpected error while updating descriptions")
+        return update_message
+
+    @staticmethod
+    def pkl_upload(upload_file, algo_name):
+        if upload_file.content_type == 'application/octet-stream':
+            pkl_obj = pickle.dumps(upload_file)
+            mongo_data = {'pkl_data': Binary(pkl_obj), 'upload_method': 'pkl'}
+            pkl_message = mdb.update(db_data, algo_name, mongo_data, "File Uploaded",
+                                     "Unexpected error while uploading pickle file")
+        else:
+            pkl_message = "Invalid File Type"
+
+        return pkl_message
+
+    @staticmethod
+    def csv_upload(upload_file):
+        global data
+        feature_list = []
+        if upload_file.content_type == 'application/vnd.ms-excel':
+            string_data = StringIO(upload_file.read().decode('utf-8'))
+            data = pd.read_csv(string_data)
+            feature_list = list(data.columns)
+            csv_message = "File Uploaded"
+        else:
+            csv_message = "Invalid File Type"
+
+        return feature_list, csv_message
+
 
 class Classification(Algorithm):
 
@@ -32,7 +61,6 @@ class Classification(Algorithm):
     context = {}
     feature_list = []
     submit_button = None
-    update_button = None
     message = None
     pkl_message = None
     pkl_change_message = None
@@ -45,7 +73,7 @@ class Classification(Algorithm):
     def get(self, request):
 
         global db_data
-        descriptions = db_data.find_one({'name': 'KNN'})
+        descriptions = mdb.find(db_data, "KNN")
         self.algo_desc = descriptions['algo_desc']
         self.ds_desc = descriptions['ds_desc']
         self.context = {'algo_desc': self.algo_desc, 'ds_desc': self.ds_desc}
@@ -54,35 +82,19 @@ class Classification(Algorithm):
     def post(self, request):
 
         global data, db_data
-        descriptions = db_data.find_one({'name': 'KNN'})
+        descriptions = mdb.find(db_data, "KNN")
         self.algo_desc = descriptions['algo_desc']
         self.ds_desc = descriptions['ds_desc']
 
         if 'update' in request.POST:
-            self.update_button = request.POST.get('update')
-            self.algo_desc = str(request.POST.get('algo_desc'))
-            self.ds_desc = str(request.POST.get('ds_desc'))
-            update_data = {'algo_desc': self.algo_desc, 'ds_desc': self.ds_desc}
-
-            try:
-                db_data.update_one({'name': 'KNN'}, {'$set': update_data})
-                self.update_message = "Descriptions Updated"
-            except:
-                self.update_message = "Unexpected error while updating descriptions"
+            self.algo_desc = request.POST.get('algo_desc')
+            self.ds_desc = request.POST.get('ds_desc')
+            self.update_message = Algorithm.description_update(self.algo_desc, self.ds_desc, 'KNN')
             self.context = {'update_message': self.update_message, 'algo_desc': self.algo_desc,
                             'ds_desc': self.ds_desc}
         elif 'pkl' in request.FILES:
             upload_file = request.FILES['pkl']
-            if upload_file.content_type == 'application/octet-stream':
-                pkl_obj = pickle.dumps(upload_file)
-                mongo_data = {'pkl_data': Binary(pkl_obj), 'upload_method': 'pkl'}
-                try:
-                    db_data.update_one({"name": "KNN"}, {"$set": mongo_data})
-                    self.pkl_message = "File Uploaded"
-                except:
-                    self.pkl_message = "Unexpected error while uploading pickle file"
-            else:
-                self.pkl_message = "Invalid File Type"
+            self.pkl_message = Algorithm.pkl_upload(upload_file, 'KNN')
             self.context = {'pkl_message': self.pkl_message, 'algo_desc': self.algo_desc,
                             'ds_desc': self.ds_desc}
         elif 'pkl_change' in request.POST:
@@ -104,24 +116,15 @@ class Classification(Algorithm):
                 graph_image = "data:%sbase64,%s" % (mime, encoded_image)
                 mongo_data = {'training_label': pkl_label, 'training_features': pkl_features,
                               'graph_image': graph_image}
-                try:
-                    db_data.update_one({"name": "KNN"}, {"$set": mongo_data})
-                    self.pkl_change_message = "File Uploaded"
-                except:
-                    self.pkl_change_message = "Unexpected error while uploading pickle changes"
+                self.pkl_change_message = mdb.update(db_data, "KNN", mongo_data, "File Uploaded",
+                                                     "Unexpected error while uploading pickle changes")
             else:
                 self.pkl_change_message = "Invalid Image Type"
             self.context = {'pkl_change_message': self.pkl_change_message, 'algo_desc': self.algo_desc,
                             'ds_desc': self.ds_desc}
         elif 'csv' in request.FILES:
             upload_file = request.FILES['csv']
-            if upload_file.content_type == 'application/vnd.ms-excel':
-                string_data = StringIO(upload_file.read().decode('utf-8'))
-                data = pd.read_csv(string_data)
-                self.feature_list = list(data.columns)
-                self.csv_message = "File Uploaded"
-            else:
-                self.csv_message = "Invalid File Type"
+            self.feature_list, self.csv_message = Algorithm.csv_upload(upload_file)
             self.context = {'csv_message': self.csv_message, 'features': self.feature_list, 'algo_desc': self.algo_desc,
                             'ds_desc': self.ds_desc}
         elif 'submit' in request.POST:
@@ -154,16 +157,13 @@ class Classification(Algorithm):
                                                   algorithm=algorithm)
                 classifier.fit(X, y)
                 y_pred = classifier.predict(X)
+                self.accuracy = round(accuracy_score(y, y_pred) * 100, 2)
 
                 pkl_obj = pickle.dumps(classifier)
                 mongo_data = {'pkl_data': Binary(pkl_obj), 'training_features': training_features,
                               'training_label': label_output, 'graph_image': graph_image, 'upload_method': 'csv'}
-                try:
-                    db_data.update_one({"name": "KNN"}, {"$set": mongo_data})
-                    self.accuracy = round(accuracy_score(y, y_pred)*100, 2)
-                    self.message = "Model Successfully Trained"
-                except:
-                    self.message = "Unexpected error while training the model."
+                self.message = mdb.update(db_data, "KNN", mongo_data, "Model Successfully Trained",
+                                          "Unexpected error while training the model.")
             else:
                 self.message = "Invalid Image Type"
 
